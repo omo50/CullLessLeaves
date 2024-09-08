6c01e2e8c6389c87f96ec5814c881ca9c17631d2
bowen <144749186+omo50@users.noreply.github.com>
2024-09-08 17:22:14 -0500

    (attempt at) fixing sodium 0.6 compat

--- src/main/java/dev/isxander/culllessleaves/compat/SodiumCompat.java
+++ src/main/java/dev/isxander/culllessleaves/compat/SodiumCompat.java
@@ -2,8 +2,8 @@ package dev.isxander.culllessleaves.compat;
 
 import dev.isxander.culllessleaves.CullLessLeaves;
 import dev.isxander.culllessleaves.config.CullLessLeavesConfig;
-import me.jellysquid.mods.sodium.client.SodiumClientMod;
-import me.jellysquid.mods.sodium.client.gui.options.storage.OptionStorage;
+import net.caffeinemc.mods.sodium.client.SodiumClientMod;
+import net.caffeinemc.mods.sodium.client.gui.options.storage.OptionStorage;
 import net.minecraft.client.MinecraftClient;
 
 public class SodiumCompat {
